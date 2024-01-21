66f8483e52b125b825ace7aef120c8a92f3f0732
Kyle Klus <kyle.klus.2@gmail.com>
2024-01-21 17:34:24 +0100

    Initial commit

---
+++ templates/pages/index.tsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import Content from "@/components/container/Content";
+import Layout from "@/components/layouts/Layout";
+
+export default function Home() {
+  return (
+    <>
+      {/* Replace with own layout */}
+      <Layout>
+        <Content className={['applyHeaderOffset'].join(' ')}>
+          {/* Insert stuff here */}
+        </Content>
+      </Layout>
+    </>
+  );
+}
